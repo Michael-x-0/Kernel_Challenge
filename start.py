36917a7dc50b985baeba8718406a5008806aa72e
Michael-x-0 <fotsomx260@gmail.com>
2023-04-15 21:14:10 +0200

    random walk

--- start.py
+++ start.py
@@ -39,25 +39,25 @@ if __name__ =="__main__":
 
     whole = train + test
     walk = RandonWalk(dic=False)
-    #walk.fit_transform(whole,n=15,lamb=None,kernel="kernel_test")
+    walk.fit_transform(whole,n=15,lamb=None,kernel="kernel_test")
     K1W = kernel_edge(whole,max_label_node,max_label_edge,var=25)
     K2W = kernel_node(whole,max_label_node, var = 25)
     K3W = kernel_len(whole, var=150)
     K5W = kernel_neighbors(whole,max_label_node,200,30)
-    #K4W = walk.load_kernel(n = len(whole),kernel='kernel_test')
+    K4W = walk.load_kernel(n = len(whole),kernel='kernel_test')
 
 
     K1 = K1W[:N_train,:][:,:N_train]
     K2 = K2W[:N_train,:][:,:N_train]
     K3 = K3W[:N_train,:][:,:N_train]
-    #K4 = K4W[:N_train,:][:,:N_train]
+    K4 = K4W[:N_train,:][:,:N_train]
     K5 = K5W[:N_train,:][:,:N_train]
 
 
     K1_test = K1W[-N_test:,:][:,:N_train]
     K2_test = K2W[-N_test:,:][:,:N_train]
     K3_test = K3W[-N_test:,:][:,:N_train]
-    #K4_test = K4W[-N_test:,:][:,:N_train]
+    K4_test = K4W[-N_test:,:][:,:N_train]
     K5_test = K5W[-N_test:,:][:,:N_train]
 
     
@@ -69,7 +69,7 @@ if __name__ =="__main__":
     #lr2 = classifier('ridge',class_weight = 'balanced',alpha=2,w=[1,1], tr = 0.01, tr2= 0.01,tr3=[0.5,0.5])
     lr1 = classifier('ridge2',class_weight = 'balanced',C=0.005,tr = 0.01, tr2= 0.01,w=[1,1])
     
-    K = K5*(0.5*K1*K2 + 0.5*K2*K3) #+ 0.08*K4
+    K = K5*(0.5*K1*K2 + 0.5*K2*K3) + 0.08*K4
     print("cross validation score for ridge")
     #score = lr1.cross_val(K ,label, cv=4, ratio= 0.8, balanced=False)
 
